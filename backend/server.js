a0ca45906fac273434b4889746304d9795b6a63c
Arheee <justinewallet1@gmail.com>
2025-05-07 14:04:25 +0200

    Fix CI/CD without Sonar bcz no analyse auto params for free version

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.use("/register", registerRoutes);
 app.use("/login", loginRoutes);
 app.listen(port, () => {
     console.log(`Serveur en écoute sur http://localhost:${port}`);
-    console.log("CI/CD teest setup OK");
+    console.log("CI/CD test setup OK");
 });
