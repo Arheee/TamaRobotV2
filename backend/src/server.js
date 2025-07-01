cce88114b848d31b764ad47872ea0fe1ab3f6119
Arheee <justinewallet1@gmail.com>
2025-07-01 15:17:46 +0200

    ajout de notes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
-const app = require("./app");
+const app = require("./app"); // importe l'app Express and le app
 
 const PORT = process.env.PORT || 3000;
 
+// On lance le serveur et attache à toutes les interfaces réseau (0000 est necessaire pour docker)
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Backend prêt sur http://api.tamarobot.localhost (via Traefik)`);
 });
